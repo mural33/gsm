9d69769429518ddf9bc8c1441f1b8bcd6e85fa96
murali <murali.yagala@alongx.com>
2023-12-17 09:55:19 +0530

    error-3

--- GSM_Webapp/deployment.py
+++ GSM_Webapp/deployment.py
@@ -14,7 +14,6 @@ CSRF_Trusted_Origins = ['*']
 # middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -24,10 +23,12 @@ MIDDLEWARE = [
 ]
 # static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, './static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Specify the directory where collectstatic will collect files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
+    os.path.join(BASE_DIR, 'static'),  # Additional directories for static files
 ]
-# # whitenoise static files
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# WhiteNoise configuration for serving static files in production
+if not DEBUG:
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # Add WhiteNoise middleware
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # media files
